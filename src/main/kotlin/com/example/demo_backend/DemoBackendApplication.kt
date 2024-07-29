6ad3c339fcd807ef154e1d6a631c0c878f04739a
yusei53 <yusei530922@gmail.com>
2024-07-29 18:15:08 +0900

    feat: ktlint

--- src/main/kotlin/com/example/demo_backend/DemoBackendApplication.kt
+++ src/main/kotlin/com/example/demo_backend/DemoBackendApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class DemoBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoBackendApplication>(*args)
+    runApplication<DemoBackendApplication>(*args)
 }
--- src/main/kotlin/com/example/demo_backend/HelloController.kt
+++ src/main/kotlin/com/example/demo_backend/HelloController.kt
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class HelloController {
     @GetMapping("/")
-    fun index(model : Model) : String {
+    fun index(model: Model): String {
         model.addAttribute("message", "Hello World")
         return "index"
     }
 }
-
